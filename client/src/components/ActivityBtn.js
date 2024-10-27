27e2341980e35a677382641a2a11d16dfa2201fd
Jason <jasontang20043@gmail.com>
2024-10-27 04:31:38 -0700

    add activities to trip and view them on detail page

--- client/src/components/ActivityBtn.js
+++ client/src/components/ActivityBtn.js
@@ -6,7 +6,15 @@ const ActivityBtn = (props) =>  {
   const [num_votes, setNumVotes] = useState(props.num_votes)
 
   const updateCount = () => {
-
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({num_votes: num_votes + 1})
+    }
+    fetch(`/api/activities/${props.id}`, options)
+    setNumVotes((prev) => prev + 1)
   }
 
   return (
